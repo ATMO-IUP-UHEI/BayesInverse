04e8e7b3fc02230cb8aa53575fe142cfc9de1235
Robert Maiwald <rmaiwald@iup.uni-heidelberg.de>
2022-09-08 16:33:42 +0200

    Release V 1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='bayesinverse',
-   version='1.0',
+   version='1.1',
    description='',
    author='Robert Maiwald',
    author_email='rmaiwald@iup.uni-heidelberg.de',
